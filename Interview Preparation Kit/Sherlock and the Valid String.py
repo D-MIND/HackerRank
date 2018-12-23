5a85fbd56866884ec48296903a631fdd5fd29976
Emanuele Moizi <37047490+EmanueLeNet@users.noreply.github.com>
2018-12-23 22:22:30 +0100

    Update Sherlock and the Valid String.py

--- Interview Preparation Kit/Sherlock and the Valid String.py
+++ Interview Preparation Kit/Sherlock and the Valid String.py
@@ -1,6 +1,6 @@
 def isValid(s):
-    counter = {}
 
+    counter = {}
     for word in s:
         try:
             counter[word] += 1
@@ -10,6 +10,7 @@ def isValid(s):
     values = list(counter.values())
     mymax = max(values)
     mymin = min(values)
+    
     if mymax == mymin or (mymax - mymin == 1 and values.count(mymax) == 1) or (mymin == 1 and values.count(mymin) == 1 and len(set(values)) == 2):
         return "YES"
     else:
